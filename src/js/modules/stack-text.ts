4c74b65c6f251eb6961404100849ada2918cfcb6
平野 大騎 <daiki.hirano@caters.co.jp>
2025-10-17 17:55:42 +0900

    update stack-text トリガー追加

--- src/js/modules/stack-text.ts
+++ src/js/modules/stack-text.ts
@@ -1,7 +1,8 @@
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default class StackText {
   // アニメーションさせるテキストのラップ要素取得
@@ -35,21 +36,26 @@ export default class StackText {
       const texts = Array.from(item.querySelectorAll("[data-stack-text]"));
 
       texts.forEach((text, index) => {
+        // splitTextの初期化
         SplitText.create(text, {
           type: "chars",
           tag: "span",
-          onSplit(self) {
-            return gsap.fromTo(
-              self.chars,
-              {
-                yPercent: index === 0 ? 0 : 110, // 最初の要素は上、複製した要素は下に配置
-              },
-              {
-                yPercent: index === 0 ? -110 : 0,
-                stagger: 0.05,
-                duration: 1,
-              }
-            );
+        });
+
+        // 初期位置
+        gsap.set(text.querySelectorAll("span"), {
+          yPercent: index === 0 ? 0 : 110, // 最初の要素は上、複製した要素は下に配置
+        });
+
+        ScrollTrigger.create({
+          trigger: item,
+          start: "top 80%",
+          onEnter: () => {
+            gsap.to(text.querySelectorAll("span"), {
+              yPercent: index === 0 ? -110 : 0,
+              stagger: 0.05,
+              duration: 1,
+            });
           },
         });
       });
